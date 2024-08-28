3565e6cb07c85a06e06daf73285d9d389def39ce
aleks100s <aleksandr100s@yandex.ru>
2024-08-28 23:24:58 +0700

    Add DeleteCardIntent

---
+++ CashbackManager/Intents/DeleteCardIntent.swift
@@ -0,0 +1,35 @@
+//
+//  DeleteCardIntent.swift
+//  CashbackManager
+//
+//  Created by Alexander on 28.08.2024.
+//
+
+import AppIntents
+import CardsService
+
+struct DeleteCardIntent: AppIntent {
+	static var title: LocalizedStringResource = "Удалить карту"
+	static var description: IntentDescription? = "Удаляет карту по названию"
+		
+	@Parameter(title: "Название карты", inputOptions: String.IntentInputOptions(keyboardType: .default))
+	var cardName: String
+	
+	@Dependency
+	private var cardsService: CardsService
+	
+	init() {}
+	
+	init(cardName: String) {
+		self.cardName = cardName
+	}
+	
+	func perform() async throws -> some ProvidesDialog {
+		guard let card = cardsService.getCard(name: cardName) else {
+			return .result(dialog: "Не получилось найти карту \(cardName)")
+		}
+		
+		cardsService.delete(card: card)
+		return .result(dialog: "Карта \"\(card.name)\" удалена!")
+	}
+}
